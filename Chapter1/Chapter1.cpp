6d31dd76dc0b736637dbbcf699b77ffe4fb3c6b4
Jan Czech <czech-j@wp.pl>
2016-12-29 10:14:05 +0100

    1st commit

---
+++ Chapter1/Chapter1.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <string>
+ 
+using namespace std;
+
+
+int main()
+{
+	int i = 1;
+	double d = 2.12;
+}
+
+
+
